508c88c6d60b3c0e4d51060ae0fd908ce2554b61
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-11-23 13:24:31 +0100

    Update Miniräknare.py

---
+++ Miniräknare.py
@@ -0,0 +1,25 @@
+
+print("Välkommen till miniräknaren.")
+print("[1] Räkna med +")
+print("[2] Räkna med -")
+print("[3] Räkna med *")
+print("[4] Valfritt arbete, om du vill")
+print("[5] Avsluta")
+while True:
+    alternativ = input("Välj ett alternativ :")
+    if alternativ == 1:
+        print("hej")
+    elif alternativ == 2:
+        print("detta var")
+    elif alternativ == 3:
+        print("för svårt")
+    elif alternativ == 4:
+        print("Jens")
+    elif alternativ == 5:
+        print("Hjälp!")
+    nånting 
+    nånting 
+    nånting
+    break
+else:
+    print("Hejdå")
