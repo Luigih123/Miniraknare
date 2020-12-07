583c29e6d108214d585e456bfc42c24544bf1f21
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-12-07 14:56:06 +0100

    Update Miniräknare.py

--- Miniräknare.py
+++ Miniräknare.py
@@ -7,16 +7,22 @@ print("[3] Räkna med *")
 print("[4] Valfritt arbete, om du vill")
 print("[5] Avsluta")
 while True:
-    alternativ = int(input("Välj ett alternativ :"))
+    alternativ = int(input("Välj ett alternativ "))
     if alternativ == 1:
-        num1 = float(input("skriv ett tal :"))
-        num2 = float(input("skriv ett tal :"))
+        num1 = float(input("skriv ett tal "))
+        num2 = float(input("skriv ett tal "))
         num3 = num1 + num2 
         print(f'{num1} + {num2} = {num3}')
     elif alternativ == 2:
-        print("")
+        num1 = float(input("skriv ett tal "))
+        num2 = float(input("skriv ett tal "))
+        num3 = num1 - num2
+        print(f'{num1} - {num2} = {num3}')
     elif alternativ == 3:
-        print("")
+        num1 = float(input("skriv ett tal "))
+        num2 = float(input("skriv ett tal "))
+        num3 = num1 * num2
+        print(f'{num1} * {num2} = {num3}')
     elif alternativ == 4:
         print("")
     elif alternativ == 5:
