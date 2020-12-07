ffb9fa69f0781f284f7e3f97b880c95d28242b56
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-12-07 14:47:52 +0100

    Update Miniräknare.py

--- Miniräknare.py
+++ Miniräknare.py
@@ -1,4 +1,5 @@
 
+
 print("Välkommen till miniräknaren.")
 print("[1] Räkna med +")
 print("[2] Räkna med -")
@@ -6,20 +7,18 @@ print("[3] Räkna med *")
 print("[4] Valfritt arbete, om du vill")
 print("[5] Avsluta")
 while True:
-    alternativ = input("Välj ett alternativ :")
+    alternativ = int(input("Välj ett alternativ :"))
     if alternativ == 1:
-        print("hej")
+        num1 = float(input("skriv ett tal :"))
+        num2 = float(input("skriv ett tal :"))
+        num3 = num1 + num2 
+        print(f'{num1} + {num2} = {num3}')
     elif alternativ == 2:
-        print("detta var")
+        print("")
     elif alternativ == 3:
-        print("för svårt")
+        print("")
     elif alternativ == 4:
-        print("Jens")
+        print("")
     elif alternativ == 5:
-        print("Hjälp!")
-    nånting 
-    nånting 
-    nånting
+        print("")
     break
-else:
-    print("Hejdå")
